22b7c2452730374597effdd03d58c8c62d75854f
dennithomas <dennithomas96@gmail.com>
2025-12-22 21:32:24 +0530

    Fix base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import Loginpage from "./Loginpage";
 import AdminPortal from "./components/Admin/AdminPortal";
 import Userportal from "./components/User/Userportal";
 
 function App() {
   return (
-    <HashRouter>
       <Routes>
         <Route path="/" element={<Loginpage />} />
         <Route path="/adminportal/*" element={<AdminPortal />} />
         <Route path="/userportal/*" element={<Userportal />} />
       </Routes>
-    </HashRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
- <HashRouter>
-  <App />
-</HashRouter>
+ <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
